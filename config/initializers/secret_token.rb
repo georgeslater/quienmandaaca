b29d29aa3507833d1c0a20967a51f2b668110d61
George Slater <georgewslater@gmail.com>
2012-06-03 16:47:32 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quienmandaaca::Application.config.secret_token = '73a325502a7925793d14a2ab0c306fe550c2b5eaa10f70590cf6a018df273505ac7f9b05999658c68a0611f762693e5c86c7bc79b2505ca87d1a843697c2b55b'
