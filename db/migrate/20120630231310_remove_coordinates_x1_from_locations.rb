a787785e093585727acef61177e5bb356ee60072
George Slater <georgewslater@gmail.com>
2012-07-08 19:45:04 -0300

    remove migration

--- db/migrate/20120630231310_remove_coordinates_x1_from_locations.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCoordinatesX1FromLocations < ActiveRecord::Migration
-  def up
-    remove_column :locations, :coordinates
-      end
-
-  def down
-    add_column :locations, :coordinates, :decimal
-  end
-end
