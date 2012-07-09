61e4dc9e723f14af2354e69bef4abb1dd799fec5
George Slater <georgewslater@gmail.com>
2012-07-08 23:53:34 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pocitos = Location.create(country: 'Uruguay', color: '##FF0000')
+
+Coordinate.create(xcoordinate: '-34.874252', ycoordinate: '-56.130262', ordering: '1', location_id: pocitos.id)
+Coordinate.create(xcoordinate: '-34.874252', ycoordinate: '-56.118292', ordering: '2', location_id: pocitos.id)
+Coordinate.create(xcoordinate: '-34.89', ycoordinate: '-56.118292', ordering: '3', location_id: pocitos.id)
+Coordinate.create(xcoordinate: '-34.89', ycoordinate: '-56.130262', ordering: '4', location_id: pocitos.id)
+
